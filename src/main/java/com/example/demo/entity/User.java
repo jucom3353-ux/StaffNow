f3650819f1b0114d5f9c0bd319d7bd75c560aaf0
상진 <sangji90@gmail.com>
2026-05-08 15:43:31 +0900

    회원가입 기능 및 USER COMPANY role 추가

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -23,12 +23,16 @@ public class User {
     // 전화번호
     private String phone;
 
+    // 비밀번호
+    private String password;
+
     // 노쇼 횟수
     private int noShowCount;
 
     // 평점
     private double rating;
 
-    // 분류
-    private String role;
+    // 사용자 역할(USER / COMPANY)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
